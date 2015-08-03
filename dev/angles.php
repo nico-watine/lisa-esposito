73e6e0a5953cc86efc38ef21439fe55d0e269a0a
nico-watine <contact@studioncreations.com>
2015-08-03 12:42:32 -0500

    Initial Commit of DEV CSS Angled Divs

---
+++ dev/angles.php
@@ -0,0 +1,32 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Angles | DEV</title>
+	<meta name="description" content="###">
+	<link type="text/plain" rel="author" href="/humans.txt">
+	
+	<link rel="stylesheet" href="<?php autoVer('/css/dev.css'); ?>"/>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+
+
+
+
+<script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
+
+</body>
+</html>
