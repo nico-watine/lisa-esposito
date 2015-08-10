90aa741146b4104c4db52139485e1245cdd19f44
nico-watine <contact@studioncreations.com>
2015-08-10 11:01:41 -0500

    Initlial Commit of Labeled static-home.php

---
+++ static-home.php
@@ -0,0 +1,117 @@
+<?php 
+	// Template name: Static Front Page
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Abstract Expressionist Paintings | Lisa Esposito, PhD - Greater Boston Area</title>
+	<meta name="description" content="Welcome to the online art gallery of Dr. Lisa Esposito! Abstract Expressionist Paintings - Located in the Greater Boston Area">
+	<link type="text/plain" rel="author" href="/humans.txt">
+	<meta name="format-detection" content="telephone=no">
+	
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body id="index">
+
+<header>
+	<div class="verbiage">
+		<h1>Lisa Esposito<span>, PhD</span></h1>	
+		<h2>Abstract Expressionist Painter</h2>
+		<nav>
+			<ul class="vertical-list">
+				<li><a href="http://lisaesposito.com/galleries.html">Galleries</a></li>
+				<!--TO BE ADDED LATER ON--> <!-- <li><a href="/upcoming-events/">Upcoming Events</a></li> --> 
+				<li><a href="http://lisaesposito.com/artist-statement.html">Artist Statement</a></li>
+				<li><a href="http://lisaesposito.com/bio.html">CV / Bio</a></li>
+			</ul>
+		</nav>
+		<div id="contact-info">
+			<div class="row">
+				<h4 id="phone"><a href="tel://832-651-3136">832-520-4743</a></h4>
+				<h4 id="email"><a href="mailto:info@lisaesposito.com" target="_top">Info@LisaEsposito.com</a></h4>
+			</div>
+			<h4>Located in the Greater Boston area</h4>
+		</div>
+	</div>
+	<div class="angled-div" id="landing"></div>
+	<aside><h4><a href="http://lisaesposito.com/galleries/new-artworks.html#!/Escape-Artist/p/52451262/category=5009182">'Escape Artist'</a></h4></aside>
+</header>
+
+<main>
+<section class="gallery-link" id="linear-abstraction">
+		<div class="verbiage">
+			<h2>GALLERY</h2>
+			<a href="http://lisaesposito.com/galleries/new-artworks.html" class="verbiage-link">
+				<h3 class="gallery-title">Linear Abstraction</h3>
+			</a>
+		</div>
+	<div class="angled-div"></div>
+	<aside><h4><a href="http://lisaesposito.com/galleries/new-artworks.html#!/Lost-in-Transit/p/51320080/category=5009182">'Lost In Transit'</a></h4></aside>
+</section>
+
+<section class="gallery-link" id="lyrical-abstraction">
+		<div class="verbiage">
+			<h2>GALLERY</h2>
+			<a href="http://lisaesposito.com/galleries/free-intuitive-spontaneous.html" class="verbiage-link">
+				<h3 class="gallery-title">Lyrical Abstraction</h3>
+			</a>
+		</div>
+		<div class="angled-div"></div>
+	<aside><h4><a href="http://lisaesposito.com/galleries/free-intuitive-spontaneous.html#!/Netherworld-SOLD/p/25990544/category=5009183">'Netherworld'</a></h4></aside>
+</section>
+
+<section class="bio">
+	<aside><h4><a href="http://lisaesposito.com/galleries/new-artworks.html#!/All-Points-West/p/48313358/category=5009182">'All Points West'</a></h4></aside>
+	<article class="verbiage">
+		<h2>Meet Lisa Esposito</h2>
+		<p>I am a self-taught, abstract expressionist artist best known for spiritually evocative, non-representational paintings. In 2005, with no prior art training, I began painting in acrylic on large-scale canvases. <br> <br>
+		Evolving as a painter, I gained gallery representation in 2006. An exhibited artist, I am inspired by the Abstract Expressionists of the early to mid-20th c. as well as Buddhism, Taoism, ancient Greek philosophy, and New England Transcendentalism. My artwork consists of vibrant, contrasting colors and abstract images drawn from 25 years of teaching Philosophy.</p>
+	</article>
+</section>
+
+<section class="quoted">
+	<h2>“The first rule is to keep an untroubled spirit. <br>The second is to look things in the face and know them for what they are.” </h2>
+	<h3>Marcus Aurelius</h3>
+	<aside><h4><a href="http://lisaesposito.com/galleries/free-intuitive-spontaneous.html#!/Back-to-Center/p/25203642/category=5009183">'Back to Center'</a></h4></aside>
+</section>
+
+</main>
+
+<footer>
+	<div class="contact-info">
+		<h5><a href="tel://832-651-3136">832-520-4743</a></h5>
+		<h5><a href="mailto:info@lisaesposito.com" target="_top">Info@LisaEsposito.com</a></h5>
+	</div>
+	<div class="footer-nav">
+		<ul>
+			<li><a href="http://lisaesposito.com/galleries.html">Galleries</a></li>
+			<!--TO BE ADDED LATER ON--> <!-- <li><a href="/upcoming-events/">Upcoming Events</a></li> --> 
+			<li><a href="http://lisaesposito.com/artist-statement.html">Artist Statement</a></li>
+			<li id="last"><a href="http://lisaesposito.com/bio.html">CV / Bio</a></li>
+		</ul>
+		<h5>&copy; Lisa Esposito, PhD | 2015</h5>
+	</div>
+</footer>
+
+
+<!--[if lt IE 9]>
+	<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+<![endif]-->
+
+</body>
+</html>
+
+
+
