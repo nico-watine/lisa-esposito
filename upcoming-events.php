c04a0f2e271927a46d7c8a80705c44ef298fd828
Nico Watine <contact@studioncreations.com>
2015-08-24 21:24:04 -0500

    Creating new 'Upcoming Events' page
    Based on .Sketch design

---
+++ upcoming-events.php
@@ -0,0 +1,37 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Abstract Expressionist Paintings | Lisa Esposito, PhD - Greater Boston Area</title>
+	<meta name="description" content="Welcome to the online art gallery of Dr. Lisa Esposito! Abstract Expressionist Paintings - Located in the Greater Boston Area">
+	<link type="text/plain" rel="author" href="/humans.txt">
+	<meta name="format-detection" content="telephone=no">
+	
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+
+
+
+
+
+
+<!--[if lt IE 9]>
+	<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+<![endif]-->
+
+</body>
+</html>
