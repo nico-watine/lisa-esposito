0fd3ce846550fb57bbb9c9aed529587a44e6db65
Nico Watine <contact@studioncreations.com>
2015-08-25 21:46:49 -0500

    Convert to WP template

---
+++ wp-content/themes/theme-name/upcoming-events.php
@@ -0,0 +1,102 @@
+<?php 
+	// Template name: Upcoming Events Page
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Abstract Expressionist Paintings | Lisa Esposito, PhD - Greater Boston Area</title>
+	<meta name="description" content="Welcome to the online art gallery of Dr. Lisa Esposito! Abstract Expressionist Paintings - Located in the Greater Boston Area">
+	<link type="text/plain" rel="author" href="/humans.txt">
+	<meta name="format-detection" content="telephone=no">
+	
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>"/>
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="<?php autoVer('/css/ie.css'); ?>"/>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="upcoming-events">
+
+<header class="outer-container">
+	<div class="row">
+		<h1 class="title">Lisa Esposito, <span>PhD</span></h1>
+		<h2 class="tag">Abstract Expressionist Paintings</h2>
+	</div>
+	<nav class="row">
+		<ul class="main-nav">
+			<li><a href="/">Home</a></li>
+			<li><a href="http://www.lisaesposito.com/galleries.html">Galleries</a></li>
+			<li class="active"><a href="#">Upcoming Events</a></li>
+			<li><a href="http://www.lisaesposito.com/artist-statement.html">Artist Statement</a></li>
+			<li><a href="http://www.lisaesposito.com/bio.html">CV/Bio</a></li>
+			<li><a href="http://www.lisaesposito.com/contact.html">Contact</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+	<h2 class="page-title">Upcoming Events</h2>
+
+	<section class="event-entry" id="ghaa-2015">
+		<aside class="feature-date">
+			<p class="header">Sep 12</p>
+			<p id="painting-title"><a href="http://www.lisaesposito.com/galleries/linear-abstraction.html#!/Under-Cover-of-Night/p/49998408/category=5009182">Under Cover of Night</a></p>
+		</aside>
+		<div class="angled-div"></div>
+		<article class="event">
+			<h3 class="header">2015 Greater Haverhill Arts <br>Association Art Festival</h3>
+			<div class="row" id="details">
+				<div class="verbiage" id="when">
+					<p id="date">September 12, 2015</p>
+					<p id="time">Saturday 10am - 4pm</p>
+				</div>
+				<div class="verbiage" id="where">
+					<p id="address">Bradford Common <br>Rte. 125 South Main Street  <br>Bradford, MA 01835</p>
+				</div>
+			</div>
+			<div class="row" id="extras">
+				<p><a href="http://www.haverhillartassociation.org/events/2015-ghaa-art-festival-september-12-2015">www.haverhillartassociation.org</a></p>
+			</div>
+		</article>
+	</section>
+
+	<section class="event-entry" id="andover-aitp-2015">
+		<aside class="feature-date">
+			<p class="header">Sep 12</p>
+			<p id="painting-title"><a href="http://www.lisaesposito.com/galleries/linear-abstraction.html#!/Travel-Markings/p/49341245/category=5009182">Travel Markings</a></p>
+		</aside>
+		<div class="angled-div"></div>
+		<article class="event">
+			<h3 class="header">2015 Andovers Artists Guild <br>‘Art in the Park’</h3>
+			<div class="row" id="details">
+				<div class="verbiage" id="when">
+					<p id="date">September 19, 2015</p>
+					<p id="time">Saturday 10am - 4pm</p>
+				</div>
+				<div class="verbiage" id="where">
+					<p id="address">The Park <br>Chestnut &amp; Bartlet Street <br>Andover, MA 01810</p>
+				</div>
+			</div>
+			<div class="row" id="extras">
+				<p><a href="http://www.andoversartistsguild.com/page/2015ArtinThePark">www.andoverartistsguild.com</a></p>
+			</div>
+		</article>
+	</section>
+</main>
+
+
+
+
+<!--[if lt IE 9]>
+	<script src="<?php autoVer('/js/ie-min.js'); ?>"></script>
+<![endif]-->
+
+</body>
+</html>
