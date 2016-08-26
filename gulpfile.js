9099032c88604776072710d90dfe89759c68ff06
Nico Watine <nico@studioncreations.com>
2016-08-26 15:27:20 -0500

    Specify style.css location

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uncss = require('gulp-uncss');
 
 gulp.task('default', function () {
-    return gulp.src('site.css')
+    return gulp.src('css/style.css')
         .pipe(uncss({
             html: ['index.html', 'posts/**/*.html', 'http://example.com']
         }))
