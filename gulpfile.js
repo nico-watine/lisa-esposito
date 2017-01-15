adc669d0f8b505a64e1506836d214b9a577e6a42
Nico Watine <nico@studioncreations.com>
2017-01-15 08:09:47 -0500

    cssnano working in gulp process!

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var uncss = require('gulp-uncss');
+var cssnano = require('gulp-cssnano');
 
 gulp.task('default', function () {
     return gulp.src('css/style.css')
         .pipe(uncss({
             html: ['http://nicos-mbp.local:5757/', 'http://nicos-mbp.local:5757/upcoming-events']
         }))
+        .pipe(cssnano())
         .pipe(gulp.dest('./css/opt'));
 });
